beb9ba2c7980de05fc03334fca17fbe037f06059
Addy897 <145797684+Addy897@users.noreply.github.com>
2024-08-27 13:22:57 +0530

    added mining

--- blockchain.c
+++ blockchain.c
@@ -10,6 +10,8 @@ typedef struct Block {
     char hash[65];
     char data[256];
     time_t timestamp;
+    int difficulty;
+    unsigned long nonce;
     struct Block* next;
 } Block;
 
@@ -22,25 +24,30 @@ void sha256(const char* str, char* hash) {
     }
 }
 
-Block* create_block(int index, const char* previous_hash, const char* data) {
+void calculate_hash(Block* block) {
+    char str[1024];
+    snprintf(str, sizeof(str), "%d%ld%s%s%lu", block->index, block->timestamp, block->previous_hash, block->data, block->nonce);
+    sha256(str, block->hash);
+}
+
+Block* create_block(int index, int difficulty, const char* previous_hash, const char* data) {
     Block* new_block = (Block*)malloc(sizeof(Block));
     new_block->index = index;
     strcpy(new_block->data, data);
     strcpy(new_block->previous_hash, previous_hash);
-    new_block->timestamp=time(0);
-    
-    char str[1024];
-    snprintf(str, sizeof(str), "%d%ld%s%s", index,new_block->timestamp, previous_hash, data);
-    
-    sha256(str, new_block->hash);
+    new_block->difficulty = difficulty;
+    new_block->nonce = 0;
+    new_block->timestamp = time(0);
     new_block->next = NULL;
-    
+
+    calculate_hash(new_block);
+
     return new_block;
 }
 
-void add_block(Block** head, int index, const char* data) {
+void add_block(Block** head, int index, int difficulty, const char* data) {
     if (*head == NULL) {
-        *head = create_block(index, "0", data);
+        *head = create_block(index, difficulty, "0", data);
         return;
     }
     
@@ -49,7 +56,7 @@ void add_block(Block** head, int index, const char* data) {
         current = current->next;
     }
     
-    current->next = create_block(index, current->hash, data);
+    current->next = create_block(index, difficulty, current->hash, data);
 }
 
 void print_blockchain(Block* head) {
@@ -59,37 +66,59 @@ void print_blockchain(Block* head) {
         printf("Previous Hash: %s\n", current->previous_hash);
         printf("Hash: %s\n", current->hash);
         printf("Timestamp: %ld\n", current->timestamp);
-        printf("Data: %s\n\n", current->data);
+        printf("Difficulty: %d\n", current->difficulty);
+        printf("Nonce: %lu\n", current->nonce);
+        printf("Data: %s\n", current->data);
+        
         current = current->next;
     }
 }
-void verify_block(Block* block){
-    char str[1024];
-    snprintf(str, sizeof(str), "%d%ld%s%s", block->index,block->timestamp, block->previous_hash, block->data);
-    char hash[65];
-    sha256(str, hash);
-    if(strncmp(block->hash,hash,strlen(hash))==0){
-        printf("Block of index %d and data %s is valid\n",block->index,block->data);
-    }else{
-        printf("Block of index %d and data %s is invalid\n",block->index,block->data);
 
+int is_valid_hash(const char* hash, int difficulty) {
+    for (int i = 0; i < difficulty; i++) {
+        if (hash[i] != '0') {
+            return 0;
+        }
     }
-
-
+    return 1;
 }
+
+void mine_block(Block* block) {
+    char str[2048];
+    char hash[65];
+    
+    while (1) {
+        snprintf(str, sizeof(str), "%d%ld%s%s%lu", block->index, block->timestamp, block->previous_hash, block->data, block->nonce);
+        
+        sha256(str, hash);
+        
+        if (is_valid_hash(hash, block->difficulty)) {
+            strcpy(block->hash, hash);
+            printf("Block mined: %s\n", hash);
+            break;
+        }
+        
+        block->nonce++;
+    }
+}
+
 int main() {
     Block* blockchain = NULL;
 
-    add_block(&blockchain, 1, "Data 1");
-    add_block(&blockchain, 2, "Data 2");
-    verify_block(blockchain->next);
+    add_block(&blockchain, 1, 4, "Data 1");
+    add_block(&blockchain, 2, 3, "Data 2");
+    add_block(&blockchain, 3, 3, "Data 3");
 
-    add_block(&blockchain, 3, "Data 3");
+    Block* current = blockchain;
+    while (current != NULL) {
+        mine_block(current);
+        current = current->next;
+    }
 
     print_blockchain(blockchain);
 
-    Block* current = blockchain;
     Block* next;
+    current = blockchain;
     while (current != NULL) {
         next = current->next;
         free(current);
@@ -98,4 +127,3 @@ int main() {
 
     return 0;
 }
-
